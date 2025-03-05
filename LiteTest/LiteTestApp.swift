1df0a05c454c3bf1627447c73391459208491843
Mario <cmuoz.altimetrik@ancestry.com>
2025-03-05 17:51:11 -0500

    Initial Commit

---
+++ LiteTest/LiteTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiteTestApp.swift
+//  LiteTest
+//
+//  Created by Carlos Mario Muñoz on 5/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LiteTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiteTestTests/LiteTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiteTestTests.swift
+//  LiteTestTests
+//
+//  Created by Carlos Mario Muñoz on 5/03/25.
+//
+
+import Testing
+@testable import LiteTest
+
+struct LiteTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
